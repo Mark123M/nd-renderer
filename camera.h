7887b1dc92fba41bd9fa9dd1265afcdce276518a
Mark123M <mark123ymm@gmail.com>
2025-07-24 05:03:55 -0400

    timestamps

--- camera.h
+++ camera.h
@@ -9,6 +9,30 @@
 #include <cassert>
 #include "util.h"
 #include "direction_light.h"
+#include <ctime>
+#include <mutex>
+
+inline std::tm localtime_xp(std::time_t timer) {
+    std::tm bt{};
+#if defined(__unix__)
+    localtime_r(&timer, &bt);
+#elif defined(_MSC_VER)
+    localtime_s(&bt, &timer);
+#else
+    static std::mutex mtx;
+    std::lock_guard<std::mutex> lock(mtx);
+    bt = *std::localtime(&timer);
+#endif
+    return bt;
+}
+
+// "YYYY-MM-DD__HH-MM-SS"
+inline std::string time_stamp(const std::string& fmt = "%F__%H-%M-%S") {
+    auto bt = localtime_xp(std::time(0));
+    char buf[64];
+    return { buf, std::strftime(buf, sizeof(buf), fmt.c_str(), &bt) };
+}
+
 
 class camera {
     int image_height;
@@ -140,7 +164,7 @@ public:
     void render() {
         initialize();
 
-        std::ofstream file{ "renders/render_OMAYGOT.ppm", std::ios::app };
+        std::ofstream file{ "renders/render_" + time_stamp() + ".ppm", std::ios::app };
 
         clock_t t0 = clock();
         file << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -157,13 +181,13 @@ public:
             }
         }
 
-        int duration = clock() - t0;
+        int duration = (clock() - t0) / 1000;
         std::clog << "\rDone " << duration << "ms                                                    \n";
 
         std::stringstream ss;
         ss << duration / 1000;
         std::string duration_string = ss.str();
-        std::ofstream metadata{ "renders/render_OMAYGOT" + duration_string + "s.metadata.txt", std::ios::app };
+        std::ofstream metadata{ "renders/render_" + time_stamp() + "_" + duration_string + "s.metadata.txt", std::ios::app };
         metadata << "time elapsed: " << duration << " ms/" << duration / 1000.0 << " s/" << duration / 60000.0 << " m" << std::endl;
         metadata << "width: " << image_width << " height: " << image_height << "  " << std::endl;
         //metadata << "samples: " << samples_per_pixel << " max depth: " << max_depth << std::endl;
