4dcf4fd41e0a96a593fcfe4bd5980b71f435fb14
Mark123M <mark123ymm@gmail.com>
2025-11-22 23:39:17 -0500

    remove frame cap

--- util.h
+++ util.h
@@ -15,7 +15,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 // program
 constexpr uint NUM_CPU_THREADS = 20;
-constexpr float SIMULATION_RATE = 100.f;
+constexpr float SIMULATION_RATE = 1000.f;
 constexpr float MIN_DELTA_TIME = 1.f / SIMULATION_RATE;
 const char* PATH_TO_SCENES = "./scenes";
 
